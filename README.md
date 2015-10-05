77ca432f78d9b4e618e09a4ebbae6dbf86650c5b
Henry S. Harrison <henry.schafer.harrison@gmail.com>
2015-10-05 13:22:17 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Gait transition test protocol
+====
+
+Run a simple trial consisting of incremental speed changes on a Trackmaster treadmill.
+
+Requires:
+
+- numpy
+- [trackmaster-python](https://github.com/hsharrison/trackmaster-python)
+
+After downloading, run `python trial.py --help` for usage information.
---
+++ trial.py
@@ -0,0 +1,44 @@
+"""Gait transition test protocol.
+
+Usage:
+  trial.py <speeds> <plateau-time> [<device>]
+  trial.py --help
+
+<speeds> specifies the speeds (in mph) in the format first:change:last
+<plateau-time> is the time (in seconds) between speed changes
+<device> (optional) is the path to the serial device connected to the treadmill.
+  The default is /dev/ttyUSB0 which usually works.
+  If not, try incrementing the integer, e.g. /dev/ttyUSB1.
+
+Example:
+  python trial.py 1:0.5:8 10
+
+This would start at 1 mph, increase speed every 10 s in increments of 0.5 mph, and stop at 8 mph.
+To do the reverse you would run:
+  python trial.py 8:-0.5:1 10
+
+"""
+from time import sleep
+import numpy as np
+from docopt import docopt
+from trackmaster import Treadmill
+
+
+def run_trial(treadmill, speeds, plateau_time):
+    for speed in speeds:
+        treadmill.speed = speed
+        print('set speed to {:.1f}'.format(speed))
+        sleep(plateau_time)
+
+
+def main(args=None):
+    args = docopt(__doc__, argv=args)
+
+    treadmill = Treadmill(args.get('<device>', '/dev/ttyUSB0'))
+
+    first, change, last = (float(x) for x in args['<speeds>'].split(':'))
+    speeds = np.arange(first, last + change, change)
+
+    plateau_time = args['<plateau-time>']
+
+    run_trial(treadmill, speeds, plateau_time)
